03490cc87795a7b9246beb830c971f8e63aeaa10
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-03-19 13:41:40 -0300

    Add blank home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   resources :streamings, only: [:new, :create, :show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'home#index'
 end
