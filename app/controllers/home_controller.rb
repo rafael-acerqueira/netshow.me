001bae9466b09c478443fabd08bc3d244dc51762
Rafael Aquino <rafael.acerqueirah@gmail.com>
2018-03-23 05:30:21 -0300

    Add home page and change styles of streaming list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
-class HomeController < ApplicationController  
+class HomeController < ApplicationController
   def index
+    @streamings = Streaming.all
+    flash[:notice] = 'Ainda não há transmissões cadastradas' if !@streamings.present?
   end
 end
--- app/helpers/streaming_helper.rb
+++ app/helpers/streaming_helper.rb
@@ -1,6 +1,6 @@
 module StreamingHelper
   def polymorphic_streaming_link(streaming)
-    if controller_name == "live_streamings"
+    if controller_name == "live_streamings" || (controller_name == "home" && streaming.started?)
       live_streaming_path(streaming)
     else
       streaming_path(streaming)
